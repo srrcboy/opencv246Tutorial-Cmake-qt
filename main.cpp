c0c3f110a50ea160ccdbcc903aab34ca43f5c755
srrcboy <b-he@163.com>
2014-12-02 21:46:43 +0800

    test opencv works under qtcreator

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
+using namespace cv;
 using namespace std;
 
-int main()
+
+int main(int argc, char **argv)
 {
-    cout << "Hello World!" << endl;
+    Mat image=imread("../../images/lena.jpg");
+    namedWindow("test",CV_WINDOW_AUTOSIZE);
+    imshow("test",image);
+    waitKey(0);
     return 0;
 }
-
